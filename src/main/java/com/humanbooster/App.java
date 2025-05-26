48f121e56eb759651d064b5f4253d2565f63e111
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-26 10:57:50 +0200

    add main application logic to initialize services and clean database

--- src/main/java/com/humanbooster/App.java
+++ src/main/java/com/humanbooster/App.java
@@ -1,7 +1,50 @@
 package com.humanbooster;
 
+import com.humanbooster.config.HibernateConfig;
+import com.humanbooster.config.ServerConfig;
+import com.humanbooster.dao.TaskDao;
+import com.humanbooster.factory.DataFactory;
+import com.humanbooster.model.Task;
+import com.humanbooster.service.TaskService;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public class App {
+
+    public static final boolean LOCAL_ENVIRONMENT = true; //Change to false if using docker
+
     public static void main(String[] args) {
 
+        System.out.println("Démarrage de l'application...");
+
+        SessionFactory sessionFactory = new HibernateConfig().getSessionFactory();
+
+        TaskService taskService = new TaskService(new TaskDao(sessionFactory));
+
+        cleanDatabase(taskService);
+
+        DataFactory dataFactory = new DataFactory(taskService);
+        dataFactory.createMultipleTask(5);
+
+        try {
+            ServerConfig serverConfig = new ServerConfig();
+            serverConfig.startServer();
+        } catch (Exception e) {
+            System.out.println("Erreur lors du démarrage du serveur : " + e.getMessage());
+        }
+
+        sessionFactory.close();
+        System.out.print("Fin du programme");
+    }
+
+    /**
+     * Cleans the database by deleting all existing tasks.
+     *
+     * @param taskService the TaskService instance used to interact with the database
+     */
+    static void cleanDatabase(TaskService taskService) {
+        List<Task> existingTasks = taskService.getAllTasks();
+        existingTasks.forEach(task -> taskService.deleteTask(task.getId()));
     }
 }
